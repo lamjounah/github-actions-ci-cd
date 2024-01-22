f310dc35d54ec0bc7ebcf52dfd91f4cde6ddf4a8
lamjounah hamza <lamjounah.prf@gmail.com>
2024-01-22 01:14:04 +0100

    code

---
+++ src/main/java/com/example/testcicd/TestCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCiCdApplication.class, args);
+	}
+
+}
